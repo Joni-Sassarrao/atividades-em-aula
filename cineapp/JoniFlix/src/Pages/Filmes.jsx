3a0e78c1d515c820e93952d0e1ee130258713591
Johnny <jp.sdc006@gmail.com>
2024-05-29 09:29:23 -0300

    JoniFlix

--- cineapp/JoniFlix/src/Pages/Filmes.jsx
+++ cineapp/JoniFlix/src/Pages/Filmes.jsx
@@ -1,20 +1,7 @@
-import data from "../../artigos.json"
-
 export default function Filmes() {
-    console.log(data)
     return (
         <>
         <h1>Filmes</h1>
-        {
-            data.map(
-                filme =>(
-                    <>
-                    <h1 key={filme.title}>{filme.title}</h1>
-                    <img src={filme.image}/>
-                    </>
-                )
-            )
-        }
         </> 
     )
 }
