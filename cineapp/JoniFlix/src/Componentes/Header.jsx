b0732233d8b7cd44c0e16500b4b2bd3a0fee5bd7
Johnny <jp.sdc006@gmail.com>
2024-06-05 08:10:24 -0300

    JoniFlix

--- cineapp/JoniFlix/src/Componentes/Header.jsx
+++ cineapp/JoniFlix/src/Componentes/Header.jsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar"
 
 export default function Header() {
     return (  
-        <header className="flex bg-[#014092] text-white p-4 mb-4 justify-between">
+        <header className="flex bg-[#014092] text-white text-3xl p-4 justify-between">
             <span>Olá, visitante!</span>
             <h1>Bem-vindo ao JoniFlix!</h1>
             <NavBar/>
--- cineapp/JoniFlix/src/Pages/Home.jsx
+++ cineapp/JoniFlix/src/Pages/Home.jsx
@@ -3,27 +3,26 @@ import data from '../../artigos.json'
 export default function Home(){
     console.log(data)
     return (
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 bg-black'>
         {
             (data.map)(
                 (filme, index) =>(
                     <>
-                    <div className='card' key={index}>
-                        <h1>{filme.title}</h1>
-                        <img src={filme.image}/>
+                    <div className='card bg-primary-fundo text-white mt-4 m-3 rounded-lg flex flex-col' key={index}>
+                        <h1 className='text-2xl m-2 mb-3'>{filme.title}</h1>
+                        <img className='w-full mb-5' src={filme.image}/>
                         <div className='tags'>
                             {
                                 filme.tags.map(tag =>(
-                                    <span className='bg-blue-800 text-white p-1 m-1' key={tag}>{tag}</span>
+                                    <span className='bg-blue-800 text-white p-1 m-2 rounded-xl ml-3.5 text-center' key={tag}>{tag}</span>
                             ))
                             }
                             <div className='texto'>
                                 {
                                     filme.text.map(text =>(
-                                        <span key={text}>{text}</span>
+                                        <span className='flex flex-row m-4 text-lg' key={text}>{text}</span>
                                     ))
                                 }
-
                             </div>
                         </div>
                     </div>
