c448c135926304c8e3944af6f90ca7de7816e658
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-01 13:41:10 -0300

    AppFireBase

---
+++ Ionic/AppFirebase/FireBaseApp/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyC2zXSKG2qXvQHbK71NpOHgFdJ1rK8dv90",
+    authDomain: "ionicfirebaseapp-643a9.firebaseapp.com",
+    projectId: "ionicfirebaseapp-643a9",
+    storageBucket: "ionicfirebaseapp-643a9.appspot.com",
+    messagingSenderId: "817031016541",
+    appId: "1:817031016541:web:fb3c318354887ce3225db4",
+    measurementId: "G-8ZDDEM8NWR"
+  }
+};
